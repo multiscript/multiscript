1633a94ae0466f619a539ef437a0b4294f41264b
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2022-07-11 21:44:46 +1000

    Add ui for version notes

--- multiscript/ui/version_form_generated.py
+++ multiscript/ui/version_form_generated.py
@@ -16,34 +16,35 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QLineEdit,
-    QSizePolicy, QSpacerItem, QTabWidget, QWidget)
+    QPlainTextEdit, QPushButton, QSizePolicy, QSpacerItem,
+    QTabWidget, QWidget)
 
 class Ui_VersionForm(object):
     def setupUi(self, VersionForm):
         if not VersionForm.objectName():
             VersionForm.setObjectName(u"VersionForm")
-        VersionForm.resize(370, 493)
+        VersionForm.setMinimumSize(QSize(370, 520))
         self.gridLayout = QGridLayout(VersionForm)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.outputsTabWidget = QTabWidget(VersionForm)
-        self.outputsTabWidget.setObjectName(u"outputsTabWidget")
+        self.nativeLangLabel = QLabel(VersionForm)
+        self.nativeLangLabel.setObjectName(u"nativeLangLabel")
 
-        self.gridLayout.addWidget(self.outputsTabWidget, 13, 0, 1, 2)
+        self.gridLayout.addWidget(self.nativeLangLabel, 6, 0, 1, 1)
 
-        self.userAbbrevLineEdit = QLineEdit(VersionForm)
-        self.userAbbrevLineEdit.setObjectName(u"userAbbrevLineEdit")
+        self.userNameLabel = QLabel(VersionForm)
+        self.userNameLabel.setObjectName(u"userNameLabel")
 
-        self.gridLayout.addWidget(self.userAbbrevLineEdit, 10, 1, 1, 1)
+        self.gridLayout.addWidget(self.userNameLabel, 9, 0, 1, 1)
 
-        self.userAbbrevLabel = QLabel(VersionForm)
-        self.userAbbrevLabel.setObjectName(u"userAbbrevLabel")
+        self.userLangLabel = QLabel(VersionForm)
+        self.userLangLabel.setObjectName(u"userLangLabel")
 
-        self.gridLayout.addWidget(self.userAbbrevLabel, 10, 0, 1, 1)
+        self.gridLayout.addWidget(self.userLangLabel, 11, 0, 1, 1)
 
-        self.nativeSectionLabel = QLabel(VersionForm)
-        self.nativeSectionLabel.setObjectName(u"nativeSectionLabel")
+        self.userLangLineEdit = QLineEdit(VersionForm)
+        self.userLangLineEdit.setObjectName(u"userLangLineEdit")
 
-        self.gridLayout.addWidget(self.nativeSectionLabel, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.userLangLineEdit, 11, 1, 1, 1)
 
         self.sourceLineEdit = QLineEdit(VersionForm)
         self.sourceLineEdit.setObjectName(u"sourceLineEdit")
@@ -51,84 +52,101 @@ class Ui_VersionForm(object):
 
         self.gridLayout.addWidget(self.sourceLineEdit, 0, 1, 1, 1)
 
+        self.notesPlainTextEdit = QPlainTextEdit(VersionForm)
+        self.notesPlainTextEdit.setObjectName(u"notesPlainTextEdit")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.notesPlainTextEdit.sizePolicy().hasHeightForWidth())
+        self.notesPlainTextEdit.setSizePolicy(sizePolicy)
+        self.notesPlainTextEdit.setMaximumSize(QSize(16777215, 60))
+
+        self.gridLayout.addWidget(self.notesPlainTextEdit, 12, 1, 2, 1)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer, 15, 0, 1, 1)
+
         self.nativeAbbrevLabel = QLabel(VersionForm)
         self.nativeAbbrevLabel.setObjectName(u"nativeAbbrevLabel")
 
         self.gridLayout.addWidget(self.nativeAbbrevLabel, 5, 0, 1, 1)
 
-        self.outputSectionLabel = QLabel(VersionForm)
-        self.outputSectionLabel.setObjectName(u"outputSectionLabel")
+        self.pushButton = QPushButton(VersionForm)
+        self.pushButton.setObjectName(u"pushButton")
 
-        self.gridLayout.addWidget(self.outputSectionLabel, 12, 1, 1, 1)
-
-        self.nativeAbbrevLineEdit = QLineEdit(VersionForm)
-        self.nativeAbbrevLineEdit.setObjectName(u"nativeAbbrevLineEdit")
-
-        self.gridLayout.addWidget(self.nativeAbbrevLineEdit, 5, 1, 1, 1)
-
-        self.nativeNameLineEdit = QLineEdit(VersionForm)
-        self.nativeNameLineEdit.setObjectName(u"nativeNameLineEdit")
-
-        self.gridLayout.addWidget(self.nativeNameLineEdit, 4, 1, 1, 1)
-
-        self.sourceLabel = QLabel(VersionForm)
-        self.sourceLabel.setObjectName(u"sourceLabel")
-
-        self.gridLayout.addWidget(self.sourceLabel, 0, 0, 1, 1)
-
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer, 14, 0, 1, 1)
-
-        self.userLangLineEdit = QLineEdit(VersionForm)
-        self.userLangLineEdit.setObjectName(u"userLangLineEdit")
-
-        self.gridLayout.addWidget(self.userLangLineEdit, 11, 1, 1, 1)
-
-        self.userSectionLabel = QLabel(VersionForm)
-        self.userSectionLabel.setObjectName(u"userSectionLabel")
-
-        self.gridLayout.addWidget(self.userSectionLabel, 8, 1, 1, 1)
-
-        self.idLineEdit = QLineEdit(VersionForm)
-        self.idLineEdit.setObjectName(u"idLineEdit")
-
-        self.gridLayout.addWidget(self.idLineEdit, 1, 1, 1, 1)
-
-        self.idLabel = QLabel(VersionForm)
-        self.idLabel.setObjectName(u"idLabel")
-
-        self.gridLayout.addWidget(self.idLabel, 1, 0, 1, 1)
-
-        self.userNameLineEdit = QLineEdit(VersionForm)
-        self.userNameLineEdit.setObjectName(u"userNameLineEdit")
-
-        self.gridLayout.addWidget(self.userNameLineEdit, 9, 1, 1, 1)
-
-        self.nativeLangLineEdit = QLineEdit(VersionForm)
-        self.nativeLangLineEdit.setObjectName(u"nativeLangLineEdit")
-
-        self.gridLayout.addWidget(self.nativeLangLineEdit, 6, 1, 1, 1)
-
-        self.userNameLabel = QLabel(VersionForm)
-        self.userNameLabel.setObjectName(u"userNameLabel")
-
-        self.gridLayout.addWidget(self.userNameLabel, 9, 0, 1, 1)
+        self.gridLayout.addWidget(self.pushButton, 13, 0, 1, 1)
 
         self.nativeNameLabel = QLabel(VersionForm)
         self.nativeNameLabel.setObjectName(u"nativeNameLabel")
 
         self.gridLayout.addWidget(self.nativeNameLabel, 4, 0, 1, 1)
 
-        self.userLangLabel = QLabel(VersionForm)
-        self.userLangLabel.setObjectName(u"userLangLabel")
+        self.sourceLabel = QLabel(VersionForm)
+        self.sourceLabel.setObjectName(u"sourceLabel")
 
-        self.gridLayout.addWidget(self.userLangLabel, 11, 0, 1, 1)
+        self.gridLayout.addWidget(self.sourceLabel, 0, 0, 1, 1)
 
-        self.nativeLangLabel = QLabel(VersionForm)
-        self.nativeLangLabel.setObjectName(u"nativeLangLabel")
+        self.nativeSectionLabel = QLabel(VersionForm)
+        self.nativeSectionLabel.setObjectName(u"nativeSectionLabel")
 
-        self.gridLayout.addWidget(self.nativeLangLabel, 6, 0, 1, 1)
+        self.gridLayout.addWidget(self.nativeSectionLabel, 2, 1, 1, 1)
+
+        self.idLineEdit = QLineEdit(VersionForm)
+        self.idLineEdit.setObjectName(u"idLineEdit")
+
+        self.gridLayout.addWidget(self.idLineEdit, 1, 1, 1, 1)
+
+        self.userSectionLabel = QLabel(VersionForm)
+        self.userSectionLabel.setObjectName(u"userSectionLabel")
+
+        self.gridLayout.addWidget(self.userSectionLabel, 8, 1, 1, 1)
+
+        self.userAbbrevLabel = QLabel(VersionForm)
+        self.userAbbrevLabel.setObjectName(u"userAbbrevLabel")
+
+        self.gridLayout.addWidget(self.userAbbrevLabel, 10, 0, 1, 1)
+
+        self.nativeNameLineEdit = QLineEdit(VersionForm)
+        self.nativeNameLineEdit.setObjectName(u"nativeNameLineEdit")
+
+        self.gridLayout.addWidget(self.nativeNameLineEdit, 4, 1, 1, 1)
+
+        self.outputsTabWidget = QTabWidget(VersionForm)
+        self.outputsTabWidget.setObjectName(u"outputsTabWidget")
+
+        self.gridLayout.addWidget(self.outputsTabWidget, 14, 0, 1, 2)
+
+        self.userAbbrevLineEdit = QLineEdit(VersionForm)
+        self.userAbbrevLineEdit.setObjectName(u"userAbbrevLineEdit")
+
+        self.gridLayout.addWidget(self.userAbbrevLineEdit, 10, 1, 1, 1)
+
+        self.nativeLangLineEdit = QLineEdit(VersionForm)
+        self.nativeLangLineEdit.setObjectName(u"nativeLangLineEdit")
+
+        self.gridLayout.addWidget(self.nativeLangLineEdit, 6, 1, 1, 1)
+
+        self.nativeAbbrevLineEdit = QLineEdit(VersionForm)
+        self.nativeAbbrevLineEdit.setObjectName(u"nativeAbbrevLineEdit")
+
+        self.gridLayout.addWidget(self.nativeAbbrevLineEdit, 5, 1, 1, 1)
+
+        self.userNameLineEdit = QLineEdit(VersionForm)
+        self.userNameLineEdit.setObjectName(u"userNameLineEdit")
+
+        self.gridLayout.addWidget(self.userNameLineEdit, 9, 1, 1, 1)
+
+        self.idLabel = QLabel(VersionForm)
+        self.idLabel.setObjectName(u"idLabel")
+
+        self.gridLayout.addWidget(self.idLabel, 1, 0, 1, 1)
+
+        self.notesLabel = QLabel(VersionForm)
+        self.notesLabel.setObjectName(u"notesLabel")
+        self.notesLabel.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+
+        self.gridLayout.addWidget(self.notesLabel, 12, 0, 1, 1)
 
         QWidget.setTabOrder(self.sourceLineEdit, self.idLineEdit)
         QWidget.setTabOrder(self.idLineEdit, self.nativeNameLineEdit)
@@ -145,17 +163,17 @@ class Ui_VersionForm(object):
 
     def retranslateUi(self, VersionForm):
         VersionForm.setWindowTitle(QCoreApplication.translate("VersionForm", u"Form", None))
-        self.userAbbrevLabel.setText(QCoreApplication.translate("VersionForm", u"Abbreviation", None))
-        self.nativeSectionLabel.setText(QCoreApplication.translate("VersionForm", u"Labels in Native Language", None))
-        self.nativeAbbrevLabel.setText(QCoreApplication.translate("VersionForm", u"Abbreviation", None))
-        self.outputSectionLabel.setText(QCoreApplication.translate("VersionForm", u"Output Settings\n"
-"", None))
-        self.sourceLabel.setText(QCoreApplication.translate("VersionForm", u"Source", None))
-        self.userSectionLabel.setText(QCoreApplication.translate("VersionForm", u"Labels in User Language", None))
-        self.idLabel.setText(QCoreApplication.translate("VersionForm", u"Version ID", None))
-        self.userNameLabel.setText(QCoreApplication.translate("VersionForm", u"Version Name", None))
-        self.nativeNameLabel.setText(QCoreApplication.translate("VersionForm", u"Version Name", None))
-        self.userLangLabel.setText(QCoreApplication.translate("VersionForm", u"Language", None))
         self.nativeLangLabel.setText(QCoreApplication.translate("VersionForm", u"Language", None))
+        self.userNameLabel.setText(QCoreApplication.translate("VersionForm", u"Version Name", None))
+        self.userLangLabel.setText(QCoreApplication.translate("VersionForm", u"Language", None))
+        self.nativeAbbrevLabel.setText(QCoreApplication.translate("VersionForm", u"Abbreviation", None))
+        self.pushButton.setText(QCoreApplication.translate("VersionForm", u"More...", None))
+        self.nativeNameLabel.setText(QCoreApplication.translate("VersionForm", u"Version Name", None))
+        self.sourceLabel.setText(QCoreApplication.translate("VersionForm", u"Source", None))
+        self.nativeSectionLabel.setText(QCoreApplication.translate("VersionForm", u"Labels in Native Language", None))
+        self.userSectionLabel.setText(QCoreApplication.translate("VersionForm", u"Labels in User Language", None))
+        self.userAbbrevLabel.setText(QCoreApplication.translate("VersionForm", u"Abbreviation", None))
+        self.idLabel.setText(QCoreApplication.translate("VersionForm", u"Version ID", None))
+        self.notesLabel.setText(QCoreApplication.translate("VersionForm", u"Notes", None))
     # retranslateUi
 
