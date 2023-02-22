ee7e552eaf64f48bfba6c84571737946c45d75eb
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2023-02-22 23:07:14 +1000

    Improve icon layout

--- multiscript.dmg.py
+++ multiscript.dmg.py
@@ -4,3 +4,8 @@ files = [ "dist/Multiscript.app" ]
 
 # Symlinks to create
 symlinks = { 'Applications': '/Applications' }
+
+icon_locations = {
+    'Multiscript.app': (100, 100),
+    'Applications': (300, 100)
+}
