9d829d2b86112339dd5395a4fbf04af512563bc3
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2024-05-07 18:03:24 +1000

    Indent template expansion checkbox to show dependence

--- multiscript/ui/plan_config_general_panel_generated.py
+++ multiscript/ui/plan_config_general_panel_generated.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'plan_config_general_panel.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.4.1
+## Created by: Qt User Interface Compiler version 6.6.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,14 +15,14 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QHBoxLayout, QSizePolicy,
+    QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_GeneralPlanConfigPanel(object):
     def setupUi(self, GeneralPlanConfigPanel):
         if not GeneralPlanConfigPanel.objectName():
             GeneralPlanConfigPanel.setObjectName(u"GeneralPlanConfigPanel")
-        GeneralPlanConfigPanel.resize(330, 68)
+        GeneralPlanConfigPanel.resize(363, 77)
         self.verticalLayout = QVBoxLayout(GeneralPlanConfigPanel)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.allowConfirmationsCheckBox = QCheckBox(GeneralPlanConfigPanel)
@@ -30,12 +30,18 @@ class Ui_GeneralPlanConfigPanel(object):
 
         self.verticalLayout.addWidget(self.allowConfirmationsCheckBox)
 
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(30, -1, -1, -1)
         self.confirmAfterTemplateExpansionCheckBox = QCheckBox(GeneralPlanConfigPanel)
         self.confirmAfterTemplateExpansionCheckBox.setObjectName(u"confirmAfterTemplateExpansionCheckBox")
 
-        self.verticalLayout.addWidget(self.confirmAfterTemplateExpansionCheckBox)
+        self.horizontalLayout.addWidget(self.confirmAfterTemplateExpansionCheckBox)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
         self.verticalLayout.addItem(self.verticalSpacer)
 
