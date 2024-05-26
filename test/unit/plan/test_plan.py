b033b760d21cdd497297dbc36bda2dcdf0cd5170
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2024-05-26 21:07:15 +1000

    Make path tests cross-platform

--- test/unit/plan/test_plan.py
+++ test/unit/plan/test_plan.py
@@ -1,25 +1,33 @@
 import unittest
 from pathlib import Path
+import platform
 
 import multiscript.plan
 
 
 class TestPlan(unittest.TestCase):
+
+    def abs_path(self, str_or_path):
+        if platform.system() == "Windows":
+            return Path("C:", str_or_path)
+        else:
+            return Path(str_or_path)
+
     def test_paths(self):
         plan = multiscript.plan.Plan()
 
         # Test absolute paths correctly become relative
-        plan.path = Path("/Path/To/Plan/plan.mplan")
-        plan.template_path = Path("/Path/To/Plan/Templates/template.txt")
-        plan.output_dir_path = Path("/Path/To/Plan/Output/")
+        plan.path = self.abs_path("/Path/To/Plan/plan.mplan")
+        plan.template_path = self.abs_path("/Path/To/Plan/Templates/template.txt")
+        plan.output_dir_path = self.abs_path("/Path/To/Plan/Output/")
         self.assertEqual(plan.template_path, Path("Templates/template.txt"))
         self.assertEqual(plan.output_dir_path, Path("Output"))
 
         # Test absolute paths correctly stay absolute
-        plan.template_path = Path("/Path/To/Somewhere/Else/Templates/template.txt")
-        plan.output_dir_path = Path("/Path/To/Somewhere/Else/Output/")
-        self.assertEqual(plan.template_path, Path("/Path/To/Somewhere/Else/Templates/template.txt"))
-        self.assertEqual(plan.output_dir_path, Path("/Path/To/Somewhere/Else/Output/"))
+        plan.template_path = self.abs_path("/Path/To/Somewhere/Else/Templates/template.txt")
+        plan.output_dir_path = self.abs_path("/Path/To/Somewhere/Else/Output/")
+        self.assertEqual(plan.template_path, self.abs_path("/Path/To/Somewhere/Else/Templates/template.txt"))
+        self.assertEqual(plan.output_dir_path, self.abs_path("/Path/To/Somewhere/Else/Output/"))
 
         # Test relative paths stay relative
         plan.template_path = Path("Templates/template.txt")
@@ -28,25 +36,25 @@ class TestPlan(unittest.TestCase):
         self.assertEqual(plan.output_dir_path, Path("Output"))
 
         # Test absolute paths calculated correctly
-        plan.path = Path("/Path/To/Plan/plan.mplan")
+        plan.path = self.abs_path("/Path/To/Plan/plan.mplan")
         plan.template_path = Path("Templates/template.txt")
         plan.output_dir_path = Path("Output")
-        self.assertEqual(plan.template_abspath, Path("/Path/To/Plan/Templates/template.txt"))
-        self.assertEqual(plan.output_dir_abspath, Path("/Path/To/Plan/Output"))
-        plan.path = Path("/Path/To/Somewhere/Else/plan.mplan")
-        self.assertEqual(plan.template_abspath, Path("/Path/To/Somewhere/Else/Templates/template.txt"))
-        self.assertEqual(plan.output_dir_abspath, Path("/Path/To/Somewhere/Else/Output"))
+        self.assertEqual(plan.template_abspath, self.abs_path("/Path/To/Plan/Templates/template.txt"))
+        self.assertEqual(plan.output_dir_abspath, self.abs_path("/Path/To/Plan/Output"))
+        plan.path = self.abs_path("/Path/To/Somewhere/Else/plan.mplan")
+        self.assertEqual(plan.template_abspath, self.abs_path("/Path/To/Somewhere/Else/Templates/template.txt"))
+        self.assertEqual(plan.output_dir_abspath, self.abs_path("/Path/To/Somewhere/Else/Output"))
 
         # Test moving the plan recalcuates paths correctly
-        plan.path = Path("/Path/To/Plan/plan.mplan")
-        plan.template_path = Path("/Path/To/Somewhere/Else/Templates/template.txt")
-        plan.output_dir_path = Path("/Path/To/Somewhere/Else/Output/")
-        self.assertEqual(plan.template_path, Path("/Path/To/Somewhere/Else/Templates/template.txt"))
-        self.assertEqual(plan.output_dir_path, Path("/Path/To/Somewhere/Else/Output/"))
-        plan.path = Path("/Path/To/Different/Location/plan.mplan")
-        self.assertEqual(plan.template_path, Path("/Path/To/Somewhere/Else/Templates/template.txt"))
-        self.assertEqual(plan.output_dir_path, Path("/Path/To/Somewhere/Else/Output/"))
-        plan.path = Path("/Path/To/Somewhere/Else/plan.mplan")
+        plan.path = self.abs_path("/Path/To/Plan/plan.mplan")
+        plan.template_path = self.abs_path("/Path/To/Somewhere/Else/Templates/template.txt")
+        plan.output_dir_path = self.abs_path("/Path/To/Somewhere/Else/Output/")
+        self.assertEqual(plan.template_path, self.abs_path("/Path/To/Somewhere/Else/Templates/template.txt"))
+        self.assertEqual(plan.output_dir_path, self.abs_path("/Path/To/Somewhere/Else/Output/"))
+        plan.path = self.abs_path("/Path/To/Different/Location/plan.mplan")
+        self.assertEqual(plan.template_path, self.abs_path("/Path/To/Somewhere/Else/Templates/template.txt"))
+        self.assertEqual(plan.output_dir_path, self.abs_path("/Path/To/Somewhere/Else/Output/"))
+        plan.path = self.abs_path("/Path/To/Somewhere/Else/plan.mplan")
         self.assertEqual(plan.template_path, Path("Templates/template.txt"))
         self.assertEqual(plan.output_dir_path, Path("Output"))
 
